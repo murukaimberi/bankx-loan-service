fac867ce03f72b1ae8c3eae0d98707fd923e2029
murukaigumbo-mberi <murukai.mberi@afrikatek.co.za>
2025-05-14 22:30:07 +0200

    main - initial project creation

---
+++ src/main/java/za/co/afrikatek/bankxloanservice/BankxLoanServiceApplication.java
@@ -0,0 +1,13 @@
+package za.co.afrikatek.bankxloanservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankxLoanServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankxLoanServiceApplication.class, args);
+    }
+
+}
